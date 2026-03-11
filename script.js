559cc3eb24e6b2c047d75ab98bbcbb8ccf05db31
Diana Petrovčíková <diana.petrovcikova@greipi.team>
2026-03-11 22:20:35 +0100

    Add images and new texts

--- script.js
+++ script.js
@@ -249,3 +249,22 @@ async function initOpeningHours() {
 }
 
 initOpeningHours();
+
+//service cards
+const serviceCards = document.querySelectorAll(".service-card");
+
+serviceCards.forEach((card) => {
+  card.addEventListener("mousemove", (e) => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    card.style.setProperty("--mx", `${x}px`);
+    card.style.setProperty("--my", `${y}px`);
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.setProperty("--mx", "50%");
+    card.style.setProperty("--my", "50%");
+  });
+});
